c612fa173604a126023b1f65fb477ecfb9491d9b
liuc27 <49946608@qq.com>
2015-03-16 16:30:37 +0900

    Update server.js
    3

--- mean-sample-ch4/server.js
+++ mean-sample-ch4/server.js
@@ -1,89 +1,223 @@
-<!doctype html>
-<html lang="en">
-<link rel="stylesheet" type="text/css" href="css/test.css">
+var express = require('express'),
+  cors = require('cors'),
+  app = express();
+app.use(cors());
+var bodyParser = require('body-parser')
+var Post = require('./models/post')
+var Type = require('./models/Types')
+var User = require('./models/user')
+var jwt = require('jwt-simple')
+var _ = require('lodash')
+var LocalStorage = require('node-localstorage').LocalStorage
+localStorage = new LocalStorage('./scratch')
 
-<head>
-  <meta charset="UTF-8">
-  <title>Example - example-input-directive-production</title>
-</head>
+var fs = require('fs')
 
-<body ng-app="inputExample">
-  <div ng-controller="ExampleController">
-    <form name="myForm">
+var secretKey = 'supersecretkey'
 
+var logger = require('morgan')
+app.use(bodyParser.json({
+  limit: '50mb'
+}))
+app.use(logger('dev'))
+app.use(express.static(__dirname))
+app.get('/api/posts', function (req, res, next) {
+  Post.find({}, function (err, posts) {
+    if (err) {
+      return next(err)
+    }
+    res.json(posts)
+  })
 
-      真名(产品名相同):
-      <input type="text" ng-model="user.name">
-      <br>类别(shopping,beauty,food,entertainment):
-      <input type="text" ng-model="user.category">
-      <br>数量:
-      <input type="number" ng-model="user.numbers">
-      <br>产品名:
-      <input type="text" ng-model="user.productName">
-      <br>产品简介:
-      <input type="text" ng-model="user.productIntroduction">
-      <br>产品详细:
-      <input type="text" ng-model="user.productDetail">
-      <br>到期日期:
-      <input type="text" ng-model="year">年
-      <input type="text" ng-model="month">月
-      <input type="text" ng-model="day">日
+})
 
-      <br>图片:
-      <input ng-change="abc()" type='file' ng-model='image' base-sixty-four-input>
+app.post('/api/posts', function (req, res, next) {
+  var idNumber;
+  Post.find({}, function (err, posts) {
+    if (err) {
+      return next(err)
+    }
+    console.log(posts.length)
+    idNumber = posts.length
+    callback(idNumber, req, res)
+  })
+})
 
-
-
-    </form>
-    <hr>
-
-    <button ng-click="sendJson()">添加</button>
-    </br>
-    </br>
-    </br>
-    </br>
-    <button ng-click="replace()">替换</button>
-    </br>
-    </br>
-    <tt id="jsonDiv">{{user}}</tt>
-    <br/>
-  </div>
-</body>
-<script src="js/angular.js"></script>
-<script src="js/angular-base64-upload.js"></script>
-<script>
-  angular.module('inputExample', ['naif.base64'])
-    .controller('ExampleController', function ($scope, $http) {
-      $scope.user = {};
-      $scope.image = {};
-      //$scope.user.productName = new Date('2015', '02', '16', '12', '33');
-
-      //var m = $scope.user.productName;
-      //console.log(m.getTime());
-      //var k = new Date();
-      //console.log(k.getTime());
-      $scope.abc = function () {
-        $scope.user.image = "data:" + $scope.image.filetype + ";base64," + $scope.image.base64;
-        console.log($scope.user)
+app.post('/api/postAll', function (req, res, next) {
+  var idNumber;
+  Post.find({}, function (err, posts) {
+    if (err) {
+      return next(err)
+    } else {
+      console.log(posts.length)
+      for (var i = 0; i < req.count; i++) {
+        idNumber = posts.length
+        callback(idNumber, req[i], res)
       }
+    }
+  })
+})
 
-      $scope.sendJson = function () {
-        $scope.user.timeLimit = new Date($scope.year, $scope.month - 1, $scope.day, '23', '59', '59');
-        $http.post("http://localhost:3000/api/posts", $scope.user).success(function (data) {
-          console.log(data)
-          alert("successfully pushed")
-        })
+var callback = function (idNumber, req, res) {
+  var post = new Post({
+    id: idNumber,
+    name: req.body.name,
+    numbers: req.body.numbers,
+    category: req.body.category,
+    productName: req.body.productName,
+    productIntroduction: req.body.productIntroduction,
+    productDetail: req.body.productDetail,
+    timeLimit: req.body.timeLimit,
+    image: req.body.image
+  })
+
+  post.save(function (err, post) {
+    if (err) {
+      return next(err)
+    }
+    res.status(201).json(post)
+  })
+}
+
+
+
+
+app.get('/api/types', function (req, res, next) {
+  Type.find(function (err, Type) {
+    if (err) {
+      return next(err)
+    }
+    res.json(Type)
+  })
+})
+
+app.post('/api/types', function (req, res, next) {
+  var type = new Type({
+    id: req.body.id,
+    name: req.body.name,
+    category: req.body.category
+  })
+  type.save(function (err, type) {
+    if (err) {
+      return next(err)
+    }
+    res.status(201).json(type)
+  })
+})
+
+app.post('/api/add', function (req, res, next) {
+
+  Post.update({
+    "name": req.body.name
+  }, {
+    $inc: {
+      numbers: -1
+    }
+  }, function () {
+    Post.find({
+      "id": req.body.couponId
+    }, function (err, data) {
+
+      console.log(data)
+      console.log(data[0].category)
+      if (err) {
+        return next(err)
+      } else if (data[0].numbers >= 0) {
+
+
+        res.send("found")
+      } else {
+        res.send("couldn't find")
       }
+    })
+  })
 
-      $scope.replace = function () {
-        $scope.user.timeLimit = new Date($scope.year, $scope.month - 1, $scope.day, '23', '59', '59');
-        $http.post("http://localhost:3000/api/replace", $scope.user).success(function (data) {
-          console.log(data)
-          if (data = "OK")
-            alert("successfully replaced")
-        })
+
+})
+
+
+app.post('/api/replace', function (req, res, next) {
+
+  Post.update({
+    "name": req.body.name
+  }, {
+    name: req.body.name,
+    numbers: req.body.numbers,
+    category: req.body.category,
+    productName: req.body.productName,
+    productIntroduction: req.body.productIntroduction,
+    productDetail: req.body.productDetail,
+    timeLimit: req.body.timeLimit,
+    image: req.body.image
+  }, function () {
+    Post.find({
+      "name": req.body.name
+    }, function (err, data) {
+
+      console.log(data)
+      console.log(data[0].category)
+      if (err) {
+        return next(err)
+      } else if (data[0].numbers >= 0) {
+        res.send("OK")
       }
-    });
-</script>
+    })
+  })
+})
 
-</html>
+app.post('/api/user', function (req, res, next) {
+  var name = req.body.username
+  var password = req.body.password
+  User.find({}, function (err, data) {
+    if (err) {
+      return next(err)
+    }
+    var userdata = findUsername(data, name)
+    if (!userdata) {
+      console.log("couldnt find user name")
+      var token = jwt.encode({
+        username: name
+      }, secretKey)
+
+      var user = new User({
+        username: req.body.username,
+        password: req.body.password,
+        phonenumber: req.body.phonenumber
+      })
+      user.save(function (err, data) {
+        if (err) {
+          return next(err)
+        }
+        console.log(data)
+        res.status(201).json(token)
+      })
+    } else res.send("already registered")
+  })
+
+})
+
+
+function findUsername(users, user) {
+  return _.find(users, {
+    "username": user
+  })
+}
+
+function validUser(user, password) {
+  return user.password === password
+}
+
+app.get('/api/user', function (req, res, next) {
+  var token = req.headers['x-auth']
+  var user = jwt.decode(token, secretKey)
+
+  res.json(user)
+
+})
+
+
+
+
+app.listen(3000, function () {
+  console.log('server listening on', 3000)
+})
