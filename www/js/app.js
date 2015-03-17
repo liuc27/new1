dfa676c7344005fae8cfdc0261b64f00068cccfc
liuc27 <49946608@qq.com>
2015-03-17 15:16:43 +0900

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -5,50 +5,50 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'starter.filters','LocalStorageModule'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'starter.filters', 'LocalStorageModule'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
+.run(function ($ionicPlatform) {
+  $ionicPlatform.ready(function () {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
 })
-.config(function (localStorageServiceProvider) {
-  localStorageServiceProvider
+  .config(function (localStorageServiceProvider) {
+    localStorageServiceProvider
       .setPrefix('myApp')
       .setNotify(true, true)
-})
-.config(function($stateProvider, $urlRouterProvider) {
+  })
+  .config(function ($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
     // setup an abstract state for the tabs directive
-      .state('tab', {
-        url: "/tab",
-        abstract: true,
-        templateUrl: "templates/tabs.html",
-        controller: 'MenuCtrl'
-      })
+    .state('tab', {
+      url: "/tab",
+      abstract: true,
+      templateUrl: "templates/tabs.html",
+      controller: 'MenuCtrl'
+    })
 
-      .state('tab.register', {
-        url: '/register',
-        views: {
-          'tab-coupon': {
-            templateUrl: 'templates/tab-register.html',
-            controller: 'MenuCtrl'
-          }
+    .state('tab.register', {
+      url: '/register',
+      views: {
+        'tab-coupon': {
+          templateUrl: 'templates/tab-register.html',
+          controller: 'MenuCtrl'
         }
-      })
+      }
+    })
       .state('tab.login', {
         url: '/login',
         views: {
@@ -79,17 +79,17 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
       }
     })
 
-      .state('tab.coupon-detail', {
-        url: '/coupon-detail/:couponId',
-        views: {
-          'tab-coupon': {
-            templateUrl: 'templates/tab-coupon-detail.html',
-            controller: 'CouponDetailCtrl'
-          }
+    .state('tab.coupon-detail', {
+      url: '/coupon-detail/:couponId',
+      views: {
+        'tab-coupon': {
+          templateUrl: 'templates/tab-coupon-detail.html',
+          controller: 'CouponDetailCtrl'
         }
-      })
+      }
+    })
 
-    .state('tab.types', {
+    .state('tab.tab-types', {
       url: '/types',
       views: {
         'tab-types': {
@@ -100,27 +100,27 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     })
 
 
-      .state('tab.tab-types-detail', {
-        url: '/type/:typeId',
-        views: {
-          'tab-types': {
-            templateUrl: 'templates/tab-types-detail.html',
-            controller: 'typeDetailCtrl'
-          }
+    .state('tab.tab-types-couponList', {
+      url: '/type/:typeId',
+      views: {
+        'tab-types': {
+          templateUrl: 'templates/tab-types-couponList.html',
+          controller: 'typeDetailCtrl'
         }
-      })
+      }
+    })
 
 
 
-      .state('tab.types-detail-coupon', {
-        url: '/tab-types-detail-coupon/:couponId',
-        views: {
-          'tab-types': {
-            templateUrl: 'templates/tab-types-detail-coupon.html',
-            controller: 'CouponDetailCtrl'
-          }
+    .state('tab.tab-types-couponList-detail', {
+      url: '/tab-types-couponList-detail/:couponId',
+      views: {
+        'tab-types': {
+          templateUrl: 'templates/tab-coupon-detail.html',
+          controller: 'CouponDetailCtrl'
         }
-      })
+      }
+    })
 
     .state('tab.favoriteList', {
       url: '/favoriteList',
@@ -132,18 +132,17 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
       }
     })
 
-      .state('tab.favoriteListCouponDetail', {
-        url: '/favoriteList/:favoriteId',
-        views: {
-          'tab-favoriteList': {
-            templateUrl: 'templates/tab-favoriteList-couponDetail.html',
-            controller: 'favoriteDetailCtrl'
-          }
+    .state('tab.favoriteListCouponDetail', {
+      url: '/favoriteList/:favoriteId',
+      views: {
+        'tab-favoriteList': {
+          templateUrl: 'templates/tab-favoriteList-couponDetail.html',
+          controller: 'favoriteDetailCtrl'
         }
-      });
+      }
+    });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/coupon');
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/coupon');
 
-});
-
+  });
